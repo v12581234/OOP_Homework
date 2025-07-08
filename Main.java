0e88080ef63ceaeed4899e6ad2b2e8677de2afb7
v12581234 <vinhnguyen125812@gmail.com>
2025-07-08 21:37:59 +0700

    Commit 21: Complete The Program And Test If It Works

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import Impl.EmployeeManagementImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        EmployeeManagementImpl employeeManagement = new EmployeeManagementImpl();
+        employeeManagement.runMenu();
     }
 }
