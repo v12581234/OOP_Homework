efb3ec7c95176cca96315f8b6436d9844bdad62e
v12581234 <vinhnguyen125812@gmail.com>
2025-07-08 21:11:44 +0700

    Commit 15: Add Functions And Methods Into Department Class

--- Entity/Department.java
+++ Entity/Department.java
@@ -1,5 +1,44 @@
-package Entity;
+package entity;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Department {
-    
+public  class Department {
+    private String departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    List<Department> listDepartment = new ArrayList<>();
+
+    public Department() {
+    }
+
+    public Department(String departmentId, String departmentName, String departmentAddress) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.departmentAddress = departmentAddress;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
 }
+
