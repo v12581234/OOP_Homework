ad19ec4759cb7f55a2f1610aa6ef1b334fd5563e
v12581234 <vinhnguyen125812@gmail.com>
2025-07-08 21:24:42 +0700

    Commit 18: Add Method Into Interface Employee_Managament_Controller

--- Controller/EmployeeManagamentController.java
+++ Controller/EmployeeManagamentController.java
@@ -1,5 +1,19 @@
 package Controller;
 
 public interface EmployeeManagamentController {
-  
+    void addEmployee();
+    void showEmployee();
+    void updateEmployee();
+    void deleteEmployee();
+    
+    // Các phương thức mới cho menu
+    void showMenu();
+    void runMenu();
+    void addDepartment();
+    void updateDepartment();
+    void showAllDepartments();
+    void showAllEmployees();
+    void showEmployeesByDepartment();
+    void showManagersAndViceManagers();
+    void calculateTotalSalaryByDepartment();
 }
